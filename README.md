41f33022a5c29592cdd1d356bab8a7b66f11b06c
Keshavarz <pleasee@change.it>
2022-12-31 19:38:26 +0330

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# A Simple React Admin Panel
+
+This project uses:
+
+- React 
+- Material UI
+- recharts
+
+# How to run?
+
+## `npm start`
+
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+
+The page will reload when you make changes.\
+You may also see any lint errors in the console.
---
+++ src/components/styles/SocialIcons.styled.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const StyledSocialIcons = styled.ul`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  a {
+    border: 1px solid #fff;
+    border-radius: 50%;
+    color: #fff;
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    margin-right: 10px;
+    padding: 8px;
+  }
+`;
